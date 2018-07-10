4807dff8d8ab49c02760efaadb7eb492babd1f2c
JackNel <jcneligan@gmail.com>
2018-07-10 14:50:47 -0400

    Add base html

--- index.php
+++ index.php
@@ -11,13 +11,23 @@
     <title>Zip Code Finder</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <h1>Zip Code Finder</h1>
+      <p>Enter a partial address to find the zip code.</p>
+      <form>
+        <div class="form-group">
+          <label for="address">Address</label>
+          <input type="text" class="form-control" id="address" placeholder="Enter partial address">
+        </div>
+        <button class="btn btn-primary">Submit</button>
+      </form>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="scripts.js"></script>
+    <!-- <script src="scripts.js"></script> -->
   </body>
 </html>
--- scripts.js
+++ scripts.js
@@ -2,6 +2,11 @@ $.ajax({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDBgfjVw_f1wLNbzqawSx-EpHksrj32Ws4",
     type: "GET",
     success: function(data) {
-        console.log(data);
+        $.each(data['results'][0]['address_components'], function(key, value) {
+            if (value['types'][0] == "postal_code") {
+                alert(value['long_name']);
+            }
+        })
+
     }
 })
