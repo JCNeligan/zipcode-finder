d487af6f727240f6680d7d035ecdc19ab0279930
JackNel <jcneligan@gmail.com>
2018-07-10 17:50:20 -0400

    hook up ajax to button

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
-$.ajax({
-    url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDBgfjVw_f1wLNbzqawSx-EpHksrj32Ws4",
-    type: "GET",
-    success: function(data) {
-        $.each(data['results'][0]['address_components'], function(key, value) {
-            if (value['types'][0] == "postal_code") {
-                alert(value['long_name']);
-            }
-        })
+$("#find-zipcode").click(function() {
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#address").val() + "&key=AIzaSyDBgfjVw_f1wLNbzqawSx-EpHksrj32Ws4",
+        type: "GET",
+        success: function(data) {
+            $.each(data['results'][0]['address_components'], function(key, value) {
+                if (value['types'][0] == "postal_code") {
+                    alert(value['long_name']);
+                }
+            })
 
-    }
+        }
+    })
 })
