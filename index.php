49931222c33d6dc3633a85a0e36789590caf7530
JackNel <jcneligan@gmail.com>
2018-07-10 22:41:30 -0400

    Add error handling and display zip code on page

--- index.php
+++ index.php
@@ -2,14 +2,10 @@
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
     crossorigin="anonymous">
-
   <title>Zip Code Finder</title>
 </head>
 
@@ -17,6 +13,7 @@
   <div class="container">
     <h1>Zip Code Finder</h1>
     <p>Enter a partial address to find the zip code.</p>
+    <div id="message"></div>
     <form>
       <div class="form-group">
         <label for="address">Address</label>
@@ -26,9 +23,7 @@
     </form>
   </div>
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
     crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-$("#find-zipcode").click(function() {
+$("#find-zipcode").click(function(e) {
+    e.preventDefault();
+
     $.ajax({
-        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("#address").val() + "&key=AIzaSyDBgfjVw_f1wLNbzqawSx-EpHksrj32Ws4",
+        url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent($("#address").val()) + "&key=AIzaSyDBgfjVw_f1wLNbzqawSx-EpHksrj32Ws4",
         type: "GET",
         success: function(data) {
-            $.each(data['results'][0]['address_components'], function(key, value) {
-                if (value['types'][0] == "postal_code") {
-                    alert(value['long_name']);
-                }
-            })
+            if (data["status"] != "OK") {
+                $("$message").html('<div class="alert alert-danger" role="alert">Zip code could not be found, please try again.</div>');
+            } else {
+                $.each(data['results'][0]['address_components'], function(key, value) {
+                    if (value['types'][0] == "postal_code") {
+                        $("$message").html('<div class="alert alert-success" role="alert"><strong>Zip Code Found! The zip code is ' + value['long_name'] + '</div>');
+                    }
+                })
+            }
 
         }
     })
